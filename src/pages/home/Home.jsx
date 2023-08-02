fd6f4e9e9302bec7937ce16dc75a964ca462d9bf
Manuella Barros <manubarrossa@gmail.com>
2023-08-02 11:03:49 -0300

    Consertando import

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Home.css'
 import Introducao from './componentes/introducao/Introducao';
 import Sobre from './componentes/sobre/Sobre';
-import Restaurantes from './componentes/restaurantes/restaurantes';
+import Restaurantes from './componentes/restaurantes/Restaurantes'
 import Banner from '../../componentes/banner/Banner';
 
 function Home() {
